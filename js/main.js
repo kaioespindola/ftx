69704b2b08d38b6579d215fd7476d0359eee5a35
Kaio Espindola <oemaildokaio@gmail.com>
2019-05-09 20:55:35 -0300

    Adicionado imóveis

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ let data = {
     "filter":{
         "cidade":[3662],
         "finalidade":"venda",
-        "tipo": ["APARTAMENTO"]
+        "tipo": ["APARTAMENTO", "CASA_SOBRADO", "Comercial"]
     },
     "paginacao":{
         "pagina": 1,
@@ -122,14 +122,14 @@ function injectImoveis(imoveis, limit) {
 
     imoveis.slice(0, limit).forEach(imovel => {
         console.log(imovel);
-        var titulo = utf8Encode(imovel.titulo);
         listaImoveis.innerHTML += `
-            <a href="#">
+            <a target="_blank" href="https://imoveis.ftxativos.com.br/">
                 <div class="imovel" style="background-image:url('${imovel.imagens[1].arquivo}')">
                     <div class="ctd">
                         <div class="dados">
-                            <h1>${utf8Decode(imovel.titulo)}</h1>
-                            <h3>${imovel.cidade}</h3>
+                            <h1>${imovel.titulo}</h1>
+                            <p>${imovel.bairro}</p>
+                            <h3>${imovel.cidade}/MS</h3>
                             <p>CLIQUE E SAIBA MAIS</p>
                         </div>
                     </div>
@@ -140,40 +140,22 @@ function injectImoveis(imoveis, limit) {
     })
 }
 
-getImoveis(ROOT_URL, data, 3);
+getImoveis(ROOT_URL, data, 6);
 
+function string_to_slug (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+  
+    // remove accents, swap ñ for n, etc
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to   = "aaaaeeeeiiiioooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
 
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
 
-function utf8Encode(unicodeString) {
-    if (typeof unicodeString != 'string') throw new TypeError('parameter ‘unicodeString’ is not a string');
-    const utf8String = unicodeString.replace(
-        /[\u0080-\u07ff]/g,  // U+0080 - U+07FF => 2 bytes 110yyyyy, 10zzzzzz
-        function(c) {
-            var cc = c.charCodeAt(0);
-            return String.fromCharCode(0xc0 | cc>>6, 0x80 | cc&0x3f); }
-    ).replace(
-        /[\u0800-\uffff]/g,  // U+0800 - U+FFFF => 3 bytes 1110xxxx, 10yyyyyy, 10zzzzzz
-        function(c) {
-            var cc = c.charCodeAt(0);
-            return String.fromCharCode(0xe0 | cc>>12, 0x80 | cc>>6&0x3F, 0x80 | cc&0x3f); }
-    );
-    return utf8String;
-}
-
-
-function utf8Decode(utf8String) {
-    if (typeof utf8String != 'string') throw new TypeError('parameter ‘utf8String’ is not a string');
-    // note: decode 3-byte chars first as decoded 2-byte strings could appear to be 3-byte char!
-    const unicodeString = utf8String.replace(
-        /[\u00e0-\u00ef][\u0080-\u00bf][\u0080-\u00bf]/g,  // 3-byte chars
-        function(c) {  // (note parentheses for precedence)
-            var cc = ((c.charCodeAt(0)&0x0f)<<12) | ((c.charCodeAt(1)&0x3f)<<6) | ( c.charCodeAt(2)&0x3f);
-            return String.fromCharCode(cc); }
-    ).replace(
-        /[\u00c0-\u00df][\u0080-\u00bf]/g,                 // 2-byte chars
-        function(c) {  // (note parentheses for precedence)
-            var cc = (c.charCodeAt(0)&0x1f)<<6 | c.charCodeAt(1)&0x3f;
-            return String.fromCharCode(cc); }
-    );
-    return unicodeString;
+    return str;
 }
